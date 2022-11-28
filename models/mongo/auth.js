814a79b1ee90c81dd799c038a300048e57a4f0f3
ManishJangid007 <87350002+ManishJangid007@users.noreply.github.com>
2022-11-28 11:43:49 +0530

    User can also use email to authenticate

--- models/mongo/auth.js
+++ models/mongo/auth.js
@@ -1,14 +1,22 @@
 const dataDriver = require('./driver');
 const bcrypt = require('bcrypt');
 
-module.exports = async function(username, password) {
+module.exports = async function(usernameOrEmail, password) {
     try {
-        let user = await dataDriver.findByUsername(username);
+        let user = await dataDriver.findByUsername(usernameOrEmail);
         if (user.length > 0) {
             user = user[0];
             if (await bcrypt.compare(password, user.password)) {
                 return user;
             } else return false;
-        } else return false;
+        } else {
+            user = await dataDriver.findByEmail(usernameOrEmail);
+            if (user.length > 0) {
+                user = user[0];
+                if (await bcrypt.compare(password, user.password)) {
+                    return user;
+                } else return false;
+            } else return false;
+        }
     } catch (e) {console.log(e);}
 }

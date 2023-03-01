95a3676365a628bfccbc2c5490d807c79ac94ff3
ManishJangid007 <87350002+ManishJangid007@users.noreply.github.com>
2023-03-01 15:37:44 +0530

    Recommiting previous commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const spn_connect = require('../models/spoonacular/connect_user');
-const bcrypt = require('bcrypt');
-const dataDriver = require('../models/mongo/driver');
-const auth = require('../models/mongo/auth');
-
 
 router.get('/test', async (req, res) => {
     console.log("Someone hit the end point", req.path);
